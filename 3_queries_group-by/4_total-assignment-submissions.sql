b6525ce8ef1a8a6eee2fa9b4afcdfb37d6dbb8c7
Lance <lanceirvingmanzanal@icloud.com>
2024-03-05 16:34:23 -0700

    Get the total number of assignment submissions for each cohort

---
+++ 3_queries_group-by/4_total-assignment-submissions.sql
@@ -0,0 +1,8 @@
+SELECT cohorts.name AS cohort, COUNT(assignment_submissions.*) AS total_submission
+FROM cohorts
+JOIN students
+ON cohorts.id = cohort_id
+JOIN assignment_submissions
+ON students.id = student_id
+GROUP BY cohort
+ORDER BY total_submission DESC;
