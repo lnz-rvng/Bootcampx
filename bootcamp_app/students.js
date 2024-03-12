65f99bbd016b2201d274f7cc356645b9511add85
Lance <lanceirvingmanzanal@icloud.com>
2024-03-12 17:35:35 -0600

    Used parametarized queries to prevent SQL injection

--- bootcamp_app/students.js
+++ bootcamp_app/students.js
@@ -9,14 +9,14 @@ const pool = new Pool({
 
 const cohort = process.argv[2];
 const limit = process.argv[3];
-
+const values = [`%${cohort}%`, limit]
 pool.query(`
 SELECT students.id, students.name, cohorts.name AS cohort_name
 FROM students
 JOIN cohorts ON cohorts.id = students.cohort_id
-WHERE cohorts.name LIKE '%${cohort}%'
-LIMIT ${limit || 5};
-`)
+WHERE cohorts.name LIKE $1
+LIMIT $2
+`, values)
 .then(res => {
   res.rows.forEach(user => {
     console.log(`${user.name} has an id of ${user.id} and was in the ${user.cohort_name} cohort`);
--- bootcamp_app/teachers.js
+++ bootcamp_app/teachers.js
@@ -14,10 +14,10 @@ pool.query(`
   JOIN teachers ON teacher_id = teachers.id
   JOIN students ON student_id = students.id
   JOIN cohorts ON cohort_id = cohorts.id
-  WHERE cohorts.name LIKE '%${cohort}%'
+  WHERE cohorts.name LIKE $1
   GROUP BY teacher, cohort
   ORDER BY teacher;
-`)
+`, [cohort])
 .then(res => {
   res.rows.forEach(user => {
     console.log(`${user.cohort}: ${user.teacher}`)
